32c448a1da7501e0b26e6507d91a8ca41401b2be
Yassine Guedidi <yassine@guedidi.com>
2017-08-13 14:23:18 +0200

    Remove local/prod dedicated constants

--- includes/defines/app.php
+++ includes/defines/app.php
@@ -10,27 +10,3 @@
 	 * @var string
 	 */
 	define('APP_VERSION', '');
-
-	/**
-	 * Host distant de l'application
-	 * @var string
-	 */
-	define('APP_WEB_HOST', getenv('AMS_APP_WEB_HOST'));
-
-	/**
-	 * URL distante de l'application
-	 * @var string
-	 */
-	define('APP_WEB_URL', 'https://'.APP_WEB_HOST.'/');
-
-	/**
-	 * Host local de l'application
-	 * @var string
-	 */
-    define('APP_LOCAL_HOST', getenv('AMS_APP_LOCAL_HOST'));
-
-	/**
-	 * URL locale de l'application
-	 * @var string
-	 */
-	define('APP_LOCAL_URL', 'http://'.APP_LOCAL_HOST.'/');
--- includes/server.php
+++ includes/server.php
@@ -1,15 +1,9 @@
 <?php
 	require_once 'defines/server.php';
 	require_once 'functions/server.php';
-	
-	/**
-	 * Host de l'application
-	 * @var string
-	 */
-	define('APP_HOST', isLocal() ? APP_LOCAL_HOST : APP_WEB_HOST);
-	
+
 	/**
 	 * URL de l'application
 	 * @var string
 	 */
-	define('APP_URL', isLocal() ? APP_LOCAL_URL : APP_WEB_URL);
+	define('APP_URL', getenv('AMS_APP_URL'));
